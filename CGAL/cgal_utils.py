cb69531ded60904c25234fcc44725ab7374c333c
Paul Baksic <paul.baksic@outlook.fr>
2025-09-05 17:50:44 +0200

    WIP

--- CGAL/cgal_utils.py
+++ CGAL/cgal_utils.py
@@ -6,14 +6,21 @@ from CGAL.CGAL_Mesh_3 import Default_mesh_criteria
 from CGAL.CGAL_Kernel import Point_3
 from CGAL import CGAL_Mesh_3
 
+import numpy as np
+import dataclasses
+from typing import List, Optional
 
 from enum import Enum
 
 
 class CGAL_Mesh_3_IO_Util(object):
+    
+    points    : np.array
+    triangles : np.array
+    tetras    : np.array
+    
     class Elem(Enum):
         POINTS = 1
-        EDGES = 2
         TRIANGLES = 3
         TETRA = 4
 
@@ -21,28 +28,95 @@ class CGAL_Mesh_3_IO_Util(object):
         self.mesh = mesh
 
     def extract(self, elems : list[Elem]):
-        pass
+        for elem in elems:
+            vnbe = {}
+            match elem:
+                case CGAL_Mesh_3_IO_Util.Elem.TRIANGLES:
+                    for elem in self.mesh.facets():
+                        for i in range(3):
+                            if not elem.vertex in vnbe:
+                                vnbe[elem.vertex(i)] = 1 
+                            else:
+                                vnbe[elem.vertex(i)] += 1 
+                case CGAL_Mesh_3_IO_Util.Elem.TETRA:
+                    for elem in self.mesh.cells():
+                        for i in range(4):
+                            if not elem.vertex in vnbe:
+                                vnbe[elem.vertex(i)] = 1 
+                            else:
+                                vnbe[elem.vertex(i)] += 1 
+
+            if CGAL_Mesh_3_IO_Util.Elem.POINTS in elems:
+                self.points = np.array([])
+            else: 
+                self.points = None
+            
+            V = {}
+            it = 0
+            for vertice in self.mesh.triangluation().finite_vertices():
+                if vertice in vnbe:
+                    V[vertice] = it
+                    if self.points is not None:
+                        self.points.append(self.mesh.triangluation().point(vertice))
+                    it += 1 
+
+            match elem:
+                case CGAL_Mesh_3_IO_Util.Elem.TRIANGLES:
+                    self.triangles = np.array([])
+                    for elem in self.mesh.facets():
+                        self.triangles.append(np.array([V[elem.vertex(0)],V[elem.vertex(1)],V[elem.vertex(2)]]))
+                case CGAL_Mesh_3_IO_Util.Elem.TETRA:
+                    self.tetras = np.array([])
+                    for elem in self.mesh.cells():
+                        self.tetras.append(np.array([V[elem.vertex(0)],V[elem.vertex(1)],V[elem.vertex(2), V[elem.vertex(3)]]]))
+                        
 
     def write_out(self, filename):
         pass
 
 
 
+class CGAL_Mesh_from(object):
+    
+    class Refiner(Enum):
+        NONE = 0
+        LLOYD = 1
+        ODT = 3
+        PERTURB = 4
 
-# Mesh generation
-c3t3 = CGAL_Mesh_3.make_mesh_3(domain, criteria, params)
+    @dataclasses.dataclass
+    class Refiner_input():
+        refiner_type : Enum
+
+        time_limit : Optional[float] = 20.0         #(ALL)              to set up, in seconds, a CPU time limit after which the optimization process is stopped. This time is measured using CGAL::Real_timer. 0 means that there is no time limit. 
+        max_iteration_number : Optional[int] = 200  #(LLOYD & ODT only) limit on the number of performed iterations. 0 means that there is no limit on the number of performed iterations.   
+        convergence : Optional[int] = 0.0           #(LLOYD & ODT only) threshold ratio of stopping criterion based on convergence: the optimization process is stopped when at the last iteration the displacement of any vertex is less than a given fraction of the length of the shortest edge incident to that vertex.
+        free_bound : Optional[bool] = False         #(LLOYD & ODT only) designed to reduce running time of each optimization iteration. Any vertex that has a displacement less than a given fraction of the length of its shortest incident edge, is frozen (i.e. is not relocated). The parameter freeze_bound gives the threshold ratio. If it is set to 0, freezing of vertices is disabled. 
+        silver_bound : Optional[bool] = False       #(PERTURB only)     is designed to give, in degrees, a targeted lower bound on dihedral angles of mesh cells. The exudation process considers in turn all the mesh cells that have a smallest dihedral angle less than sliver_bound and tries to make them disappear by weighting their vertices. The optimization process stops when every cell in the mesh achieves this quality. The default value is 0 and means that there is no targeted bound: the exuder then runs as long as it can improve the smallest dihedral angles of the set of cells incident to some vertices. 
+        
+
+    IOUtil : CGAL_Mesh_3_IO_Util
+
+    def __init__(self):
+        pass
+
+    def generate(self):
+        pass
 
 
-for cell in c3t3.cells():
-    for i in range(4):
-        id = c3t3.index(cell.vertex(i))
-        if id.is_of_first_type():
-            print(id.get_first())
-        else:
-            print(id.get_second())
-
-
-# Output
-c3t3.write_to_file("out_1.mesh")
+    def __getattr__(self, name):
+        match name:
+            case "points":
+                return self.IOUtil.points
+            case "triangles":
+                return self.IOUtil.triangles
+            case "tetras":
+                return self.IOUtil.tetras
+            case _:
+                if name in self.__dict__:
+                    return self.__dict__[name]
+                else:
+                    raise AttributeError()
+    
 
 
--- CGAL/mesh_from_polyhedron.py
+++ CGAL/mesh_from_polyhedron.py
@@ -9,55 +9,91 @@ from CGAL import CGAL_Mesh_3
 import os
 from vedo import Mesh
 
+from cgal_utils import CGAL_Mesh_from, CGAL_Mesh_3_IO_Util
+
+class CGAL_Mesh_from_polyhedron(CGAL_Mesh_from):
+    def __init__(self, filename = None, polyhedron = None):
+        if(filename is None and polyhedron is None):
+            raise ValueError("Need either a filename to load or a CGAl polyhedron already build")
+        if(filename is not None):
+            mesh = Mesh(filename)
+            self.polyhedron = Polyhedron_3()
+            pm =  Polyhedron_modifier()
+
+            pm.begin_surface(3,1)
+            for point in mesh.points:
+                pm.add_vertex(Point_3(point[0],point[1],point[2]))
+
+            for cell in mesh.cells:
+                pm.begin_facet()
+                pm.add_vertex_to_facet(int(cell[0]))
+                pm.add_vertex_to_facet(int(cell[1]))
+                pm.add_vertex_to_facet(int(cell[2]))
+                pm.end_facet()
+
+
+            self.polyhedron.delegate(pm) 
+        else:
+            self.polyhedron = polyhedron
+        
+        print(f"Polyhedron (vertices, facets, edges) = {(self.polyhedron.size_of_vertices(), self.polyhedron.size_of_facets(), self.polyhedron.size_of_halfedges()/2)}")
+
+
+
+
+
+    def generate(self, criteria = Default_mesh_criteria()
+                                      .facet_angle(25)
+                                      .facet_size(0.15)
+                                      .facet_distance(0.008)
+                                      .cell_radius_edge_ratio(3),
+                    refiner = CGAL_Mesh_from.Refiner_input(refiner_type=CGAL_Mesh_from.Refiner.NONE)):
+        
+        # Create domain
+        domain = Polyhedral_mesh_domain_3(self.polyhedron)
+        params = Mesh_3_parameters()
+        params.no_exude()
+        params.no_perturb()
+        match refiner.refiner_type:
+            case CGAL_Mesh_from.Refiner.LLOYD:
+                params.set_lloyd(refiner.time_limit, refiner.max_iteration_number, refiner.convergence, refiner.free_bound)
+            case CGAL_Mesh_from.Refiner.ODT:
+                params.set_odt(refiner.time_limit, refiner.max_iteration_number, refiner.convergence, refiner.free_bound)
+            case CGAL_Mesh_from.Refiner.PERTURB:
+                params.set_perturb(refiner.time_limit, refiner.silver_bound)
+
+
+        # Mesh generation
+        c3t3 = CGAL_Mesh_3.make_mesh_3(domain, criteria, params)
+
+
+        self.IOUtil = CGAL_Mesh_3_IO_Util(c3t3)
+        #self.IOUtil.extract([CGAL_Mesh_3_IO_Util.Elem.POINTS, CGAL_Mesh_3_IO_Util.Elem.TETRA])
+
+    def write_out(self, filename):
+        self.IOUtil.write_out(filename)
+        
+
+
+#'data/mesh/torus.obj'
+
+
 
 #writer = vtk.vtkPolyDataWriter()
 #writer.SetFileVersion(42)
 
-mesh = Mesh('data/mesh/torus.obj')
 
 # Create input polyhedron
-polyhedron = Polyhedron_3()
-pm =  Polyhedron_modifier()
-
-pm.begin_surface(3,1)
-for point in mesh.points:
-    pm.add_vertex(Point_3(point[0],point[1],point[2]))
-
-for cell in mesh.cells:
-    pm.begin_facet()
-    pm.add_vertex_to_facet(int(cell[0]))
-    pm.add_vertex_to_facet(int(cell[1]))
-    pm.add_vertex_to_facet(int(cell[2]))
-    pm.end_facet()
 
 
-polyhedron.delegate(pm)
-print(f"(v,f,e) = {(polyhedron.size_of_vertices(), polyhedron.size_of_facets(), polyhedron.size_of_halfedges()/2)}")
 
-# Create domain
-domain = Polyhedral_mesh_domain_3(polyhedron)
-params = Mesh_3_parameters()
-params.no_exude()
-params.no_perturb()
+#for cell in c3t3.cells():
+#    for i in range(4):
+#        id = c3t3.index(cell.vertex(i))
+#        if id.is_of_first_type():
+#            print(id.get_first())
+#        else:
+#            print(id.get_second())
 
-# Mesh criteria (no cell_size set)
-criteria = Default_mesh_criteria()
-criteria.facet_angle(25).facet_size(0.15).facet_distance(
-    0.008).cell_radius_edge_ratio(3)
-# Mesh generation
-c3t3 = CGAL_Mesh_3.make_mesh_3(domain, criteria, params)
-
-
-for cell in c3t3.cells():
-    for i in range(4):
-        id = c3t3.index(cell.vertex(i))
-        if id.is_of_first_type():
-            print(id.get_first())
-        else:
-            print(id.get_second())
-
-
-# Output
-c3t3.write_to_file("out_1.mesh")
 
 
